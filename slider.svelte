808508068b7296531f2984e29e24940d2f913bc1
Timo Anttila <14306628+timoanttila@users.noreply.github.com>
2021-04-16 09:40:39 +0300

    Create slider.svelte

---
+++ slider.svelte
@@ -0,0 +1,47 @@
+<script>
+	export let images;
+	export let title;
+
+	$: activeImage = 0;
+
+	function changeImg(e) {
+		let i = $data.img.length;
+		if (e == 1) {
+			if (activeImage == 0) activeImage = i - 1;
+			else activeImage -= 1;
+		} else {
+			if (i == activeImage + 1) activeImage = 0;
+			else activeImage += 1;
+		}
+	}
+</script>
+
+<img class="bgw" src={images[activeImage]} alt={title}/>
+<button class="arrow arrowLeft" on:click={() => changeImg(1)}>&lsaquo;</button>
+<button class="arrow arrowRight" on:click={() => changeImg(2)}>&rsaquo;</button>
+
+<style>
+	.arrow {
+		background-color: transparent;
+		color: #fff;
+		position: absolute;
+		top: 0;
+		width: 70px;
+		height: 100%;
+		font-size: 7rem
+	}
+
+	.arrowLeft { left: 0 }
+
+	.arrowRight { right: 0 }
+
+	button { cursor: pointer }
+
+	.rel {
+		position: relative
+	}
+
+	.bgw {
+		background-color: #fff
+	}
+</style>
