7adbd090d88908424c471707b2500c94aaae3ab9
Timo Anttila <14306628+timoanttila@users.noreply.github.com>
2021-05-11 00:55:34 +0300

    Example with alt texts

---
+++ example.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Gallery from './gallery.svelte';
+  let images = [
+    {
+      image: "https://timoanttila.com/images/gallery/2018-deli-rumakuru-lapland.webp",
+      alt: "Image Alt 1"
+    }, {
+      image: "https://timoanttila.com/images/gallery/20200922-deli-timo-nokia.webp",
+      alt: "Image Alt 2"
+    }
+  ];
+</script>
+
+<Gallery images={images}/>
