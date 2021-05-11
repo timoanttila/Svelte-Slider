4cf73f981fe7aebfaa5a1eb8f2b3c3f89f13b6c1
Timo Anttila <14306628+timoanttila@users.noreply.github.com>
2021-05-11 23:10:41 +0300

    Update example.svelte

--- example.svelte
+++ example.svelte
@@ -1,14 +1,17 @@
 <script>
-  import Gallery from './lightboxSlider.svelte';
-  let images = [
-    {
-      image: "https://timoanttila.com/images/gallery/2018-deli-rumakuru-lapland.webp",
-      alt: "Image Alt 1"
-    }, {
-      image: "https://timoanttila.com/images/gallery/20200922-deli-timo-nokia.webp",
-      alt: "Image Alt 2"
-    }
-  ];
+	import Slider from './lightboxSlider.svelte';
+	import Gallery from './lightbox.svelte';
+	let images = [
+		{
+			image: "https://timoanttila.com/images/gallery/2018-deli-rumakuru-lapland.webp",
+			alt: "Image Alt 1"
+		}, {
+			image: "https://timoanttila.com/images/gallery/20200922-deli-timo-nokia.webp",
+			alt: "Image Alt 2"
+		}
+	];
 </script>
+
+<Slider images={images}/>
 
 <Gallery images={images}/>
